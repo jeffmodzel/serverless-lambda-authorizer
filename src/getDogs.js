44a325a7d18b9546918014df5a0c8d153450e0e7
Jeff Modzel <jemod@yahoo.com>
2020-04-08 17:03:28 -0400

    working

---
+++ src/getDogs.js
@@ -0,0 +1,20 @@
+
+module.exports.handler = function(event, context, callback) {
+  console.log(event); // Contains incoming request data (e.g., query params, headers and more)
+
+  const data = [
+      {"name" : "Stella", "breed" : "Yorkie" },
+      {"name" : "Spike", "breed" : "Golden Retriever" },
+      {"name" : "Bella", "breed" : "Boxer" }
+  ];
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      'x-custom-header': 'My Header Value',
+    },
+    body: JSON.stringify(data),
+  };
+
+  callback(null, response);
+};
